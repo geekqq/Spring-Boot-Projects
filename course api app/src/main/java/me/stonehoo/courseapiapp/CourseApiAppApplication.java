cde228749813202256c6da76482fc8e818c9873e
geekqq <jinwei1014@gmail.com>
2023-04-17 12:06:11 -0700

    first commit

---
+++ course api app/src/main/java/me/stonehoo/courseapiapp/CourseApiAppApplication.java
@@ -0,0 +1,13 @@
+package me.stonehoo.courseapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiAppApplication.class, args);
+    }
+
+}
---
+++ course api app/src/test/java/me/stonehoo/courseapiapp/CourseApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.stonehoo.courseapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
